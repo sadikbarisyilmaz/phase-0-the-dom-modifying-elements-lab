9ccd5e6c28a5afdeb62595c06554f6a4ae6ac208
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-04-10 13:04:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const main = document.querySelector("#main")
+main.remove()
+
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id","victory")
+newHeader.innerHTML = "Barış is the champion"
+// const main = document.querySelector("#main")
+// const main = document.querySelector("#main")
+// const main = document.querySelector("#main")
